45e7dff4e03dd38771e76503c54fd2c485ddbad4
unknown <mvleite0908@gmail.com>
2023-12-11 18:35:02 -0300

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
+import * as cors from 'cors';
 
 // Load environment variables from .env file
 config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors());
   await app.listen(3000);
 }
 bootstrap();
