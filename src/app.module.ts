9bd15dfd9ba9a3f965fe07b9527f71bfb5077658
unknown <mvleite0908@gmail.com>
2024-01-08 19:53:26 -0300

    feat: ajustes docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,20 @@ import { TransactionModule } from './modules/transaction/transaction.module';
 import { DatabaseModule } from './modules/database/database.module';
 import { CategoryModule } from './modules/category/category.module';
 import { ChartsModule } from './modules/charts/charts.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [HttpModule, TransactionModule, DatabaseModule, CategoryModule, ChartsModule],
+  imports: [
+    HttpModule,
+    TransactionModule,
+    DatabaseModule,
+    CategoryModule,
+    ChartsModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'fe-financial-helper', 'dist'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
